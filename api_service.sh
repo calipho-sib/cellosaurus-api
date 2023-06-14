8966a1264c074b1072c27eefdb5b2653439548dc
Pierre-André <pierre-andre.michel@isb-sib.ch>
2023-06-14 15:20:18 +0200

    Made script for stopping API more reliable

--- api_service.sh
+++ api_service.sh
@@ -28,39 +28,55 @@ echo "pattern   : $pattern"
 
 if [ "$server" == "" ]; then
   server=$host
-  echo "server    : CELLAPI_SERVER undefined, set to default hostname"
+  echo "Server    : CELLAPI_SERVER undefined, set to default hostname"
 fi
-echo "server    : $server"
+echo "Server    : $server"
 
 if [ "$action" == "start" ]; then
-  echo "starting API"
+  echo "Starting API"
   nohup $mypython main.py -s $server -p $port -r $scope -l False > cellapi.log 2>&1 &
+  pid=$(pgrep -f $pattern)
+  echo "Started, API process has pid $pid"
   echo done
 
-elif [ "$action" == "status" ]; then    
+elif [ "$action" == "status" ]; then
   pid=$(pgrep -f $pattern)
-  if [ "$pid" == "" ]; then  
-    echo "Stopped, found no API process running"
+  if [ "$pid" == "" ]; then
+    echo "Stopped, no API process running"
     exit
   else
-    echo "Started, found API process running, pid $pid"
+    echo "Running, API process has pid $pid"
     exit
   fi
 
 elif [ "$action" == "stop" ]; then
-  echo "stopping API"
+  echo "Stopping API"
   pid=$(pgrep -f $pattern)
   if [ "$pid" == "" ]; then
     echo "Oups, found no process to kill"
     exit 3
   else
-    echo killing pid $pid
+    echo Killing pid $pid
     kill $pid
-    echo done
+    sleep 1
+    pid=$(pgrep -f $pattern)
+    if [ "$pid" != "" ]; then  
+      echo "Could not stop process $pid smoothly"
+      echo "Will use kill -9 in 5 seconds instead..."
+      sleep 5
+      children=$(pgrep -P $pid)
+      echo "Killing main process $pid"
+      kill -9 $pid
+      for p in $children; do
+        echo "Killing sub process $p"
+        kill -9 $p
+      done
+    fi
+    echo Done
   fi
 
 else
   echo "Error: Don't know what to do, usage: $0 start|stop|status"
-  exit 2	
+  exit 2
 fi
 
